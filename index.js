b0600d6477b9afc31d0ec1233f8362c76584f985
Stacy Alyiela <salyiela@icloud.com>
2024-03-26 15:26:46 +0300

    My first conditional lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return "This one is on me!";
+
+  } else if (ride >= 400 && ride <=2000){
+    return "That will be twenty bucks.";
+
+  } else if (ride >= 2000 && ride <= 2500){
+    return "I will gladly take your thirty bucks.";
+
+  }else {
+    return "No can do.";
+  }
+
+  
+
+
+  }
+
+
+function ternaryCheckCity(isCity){
+  // Write your code here!
+  const city = 'NYC';
+  return (isCity === city ? "Ok, sounds good." : "No go.");
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case 'thanks for everything' :
+      return 'Bye.'
+    default:
+      }
 }
